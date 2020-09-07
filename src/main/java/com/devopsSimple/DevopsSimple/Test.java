8dc514fc20df4bf96b6ee1561e9477a74f895962
traoremoussa <moussa.traoreinfo@gmail.com>
2020-09-07 02:35:18 +0200

    fix test push

---
+++ src/main/java/com/devopsSimple/DevopsSimple/Test.java
@@ -0,0 +1,11 @@
+package com.devopsSimple.DevopsSimple;
+
+import java.util.function.BiFunction;
+
+public class Test {
+
+    BiFunction<Integer, Integer, Long> additionner = (val1, val2) -> (long) val1 + val2;
+    // interface                                     methode apply 
+    // dans cette interface il y a (apply)  qu'il faut appeler 
+
+}
